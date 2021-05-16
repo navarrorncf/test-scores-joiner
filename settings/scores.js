53fd9b475f6acb8fc6194b78ea2b01198aa8f3c4
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-05-16 09:41:37 -0300

    added scores parser

---
+++ settings/scores.js
@@ -0,0 +1,46 @@
+const load = require("../utils/load");
+const normalizeString = require("../utils/normalizeString");
+const validateGroup = require("./groups");
+
+const bimester = 1;
+const regex = new RegExp(`\.${bimester}\.`);
+
+const loadFile = (fileName) => ({
+  [fileName.split(".")[0]]: normalizeString(
+    load(__dirname, "scores", fileName)
+  ),
+});
+
+const pluckCode = (email) => {
+  try {
+    let code = email.match(/[0-9]+/)[0];
+    if (code) return code;
+
+    throw new Error();
+  } catch (e) {
+    e.message = `Invalid email format: ${email}`;
+  }
+};
+
+const parseLine = (line, key) => {
+  let [email, group, name, score] = line.split(",").map((str) => str.trim());
+  group = validateGroup(group);
+  const code = pluckCode(email);
+
+  return { code, name: name.replace(/\./g, ""), email, [key]: score, group };
+};
+
+const parseScores = (rawObject) => {
+  let [key, val] = [...Object.keys(rawObject), ...Object.values(rawObject)];
+  val = val.split("\n").map((line) => parseLine(line, key));
+  return val;
+};
+
+const gradesData = require("fs")
+  .readdirSync(`${__dirname}/scores`)
+  .filter((file) => /csv$/.test(file) && regex.test(file))
+  .map(loadFile)
+  .map(parseScores)
+  .reduce((acc, cur) => [...acc, ...cur], []);
+
+module.exports = gradesData;
